ce961d6c99619da45947d56085acf1741c48f1ff
marial711 <marialeyva782@gmail.com>
2020-05-12 16:09:29 -0400

    added rotation per click

--- js/app.js
+++ js/app.js
@@ -2,34 +2,48 @@ let dataA = document.getElementsByTagName(`td`).item(0);
 let dataB = document.getElementsByTagName(`td`).item(1);
 let dataC = document.getElementsByTagName(`td`).item(2);
 let dataD = document.getElementsByTagName(`td`).item(3);
-let allData = document.querySelectorAll(`table`);
 
-dataA.addEventListener(`click`, () => {
-    document.getElementsByTagName(`td`).item(0).className = `rotate-data`;
-    console.log(dataA.compareDocumentPosition(allData));
-    if(dataA.compareDocumentPosition(allData)& dataA.DOCUMENT_POSITION_CONTAINED_BY){
-        document.getElementsByTagName(`td`).item(0).className = `reverse-rotate`;
-    }
-});
-dataB.addEventListener(`click`, () => {
-    document.getElementsByTagName(`td`).item(1).className = `rotate-data`;
-    if(dataB.compareDocumentPosition(allData)& dataB.DOCUMENT_POSITION_CONTAINED_BY){
-        document.getElementsByTagName(`td`).item(1).className = `reverse-rotate`;
-    }
+window.onload = () => {
+    dataA.addEventListener(`click`, () => {
+        dataA.className = `rotate-data`;
+        if((dataB.className === `rotate-data`)){
+            dataB.className = `reverse-rotate`;
+        }else if((dataC.className === `rotate-data`)){
+            dataC.className = `reverse-rotate`;
+        }else if((dataD.className === `rotate-data`)){
+            dataD.className = `reverse-rotate`;
+        }
+    });
+    dataB.addEventListener(`click`, () => {
+        dataB.className = `rotate-data`;
+        if((dataA.className === `rotate-data`)){
+            dataA.className = `reverse-rotate`;
+        }else if((dataC.className === `rotate-data`)){
+            dataC.className = `reverse-rotate`;
+        }else if((dataD.className === `rotate-data`)){
+            dataD.className = `reverse-rotate`;
+        }
+    });
 
-});
+    dataC.addEventListener(`click`, () => {
+        dataC.className = `rotate-data`;
+        if((dataA.className === `rotate-data`)){
+            dataA.className = `reverse-rotate`;
+        }else if((dataB.className === `rotate-data`)){
+            dataB.className = `reverse-rotate`;
+        }else if((dataD.className === `rotate-data`)){
+            dataD.className = `reverse-rotate`;
+        }
+    });
 
-dataC.addEventListener(`click`, () => {
-    document.getElementsByTagName(`td`).item(2).className = `rotate-data`;
-    if(dataC.compareDocumentPosition(allData)& dataC.DOCUMENT_POSITION_CONTAINED_BY){
-        document.getElementsByTagName(`td`).item(2).className = `reverse-rotate`;
-    }
-});
-
-dataD.addEventListener(`click`, () => {
-    document.getElementsByTagName(`td`).item(3).className = `rotate-data`;
-    if(dataC.compareDocumentPosition(allData)& dataC.DOCUMENT_POSITION_CONTAINED_BY){
-        document.getElementsByTagName(`td`).item(3).className = `reverse-rotate`;
-    }
-
-});
+    dataD.addEventListener(`click`, () => {
+        dataD.className = `rotate-data`;
+        if((dataA.className === `rotate-data`)){
+            dataA.className = `reverse-rotate`;
+        }else if((dataB.className === `rotate-data`)){
+            dataB.className = `reverse-rotate`;
+        }else if((dataC.className === `rotate-data`)){
+            dataC.className = `reverse-rotate`;
+        }
+    });
+};
