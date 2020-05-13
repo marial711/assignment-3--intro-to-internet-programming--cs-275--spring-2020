552f3146dedecca3d6e384eb4b53cdd252da275f
marial711 <marialeyva782@gmail.com>
2020-05-13 00:06:41 -0400

    Final Edits

--- js/app.js
+++ js/app.js
@@ -2,63 +2,52 @@ let dataA = document.getElementsByTagName(`td`).item(0);
 let dataB = document.getElementsByTagName(`td`).item(1);
 let dataC = document.getElementsByTagName(`td`).item(2);
 let dataD = document.getElementsByTagName(`td`).item(3);
-
 window.onload = () => {
     dataA.addEventListener(`click`, () => {
-        dataA.style.color = `blue`;
-        dataA.className = `rotate-data`;
-        if((dataB.className === `rotate-data`& dataB.style.color === `blue`)){
-            dataB.style.color = `black`;
+        if((dataB.className === `rotate-data`)){
             dataB.className = `reverse-rotate`;
-        }else if((dataC.className === `rotate-data`& dataC.style.color === `blue`)){
-            dataC.style.color = `black`;
+        }
+        if((dataC.className === `rotate-data`)){
             dataC.className = `reverse-rotate`;
-        }else if((dataD.className === `rotate-data`& dataD.style.color === `blue`)){
-            dataD.style.color = `black`;
+        }
+        if((dataD.className === `rotate-data`)){
             dataD.className = `reverse-rotate`;
         }
     });
     dataB.addEventListener(`click`, () => {
-        dataB.style.color = `blue`;
         dataB.className = `rotate-data`;
-        if((dataA.className === `rotate-data` & dataA.style.color === `blue`)){
-            dataA.style.color = `black`;
+        if((dataA.className === `rotate-data`)){
             dataA.className = `reverse-rotate`;
-        }else if((dataC.className === `rotate-data`& dataC.style.color === `blue`)){
-            dataC.style.color = `black`;
+        }
+        if((dataC.className === `rotate-data`)){
             dataC.className = `reverse-rotate`;
-        }else if((dataD.className === `rotate-data`& dataD.style.color === `blue`)){
-            dataD.style.color = `black`;
+        }
+        if((dataD.className === `rotate-data`)){
             dataD.className = `reverse-rotate`;
         }
     });
 
     dataC.addEventListener(`click`, () => {
-        dataC.style.color = `blue`;
         dataC.className = `rotate-data`;
-        if((dataA.className === `rotate-data`& dataA.style.color === `blue`)){
-            dataA.style.color = `black`;
+        if((dataA.className === `rotate-data`)){
             dataA.className = `reverse-rotate`;
-        }else if((dataB.className === `rotate-data`& dataB.style.color === `blue`)){
-            dataB.style.color = `black`;
+        }
+        if((dataB.className === `rotate-data`)){
             dataB.className = `reverse-rotate`;
-        }else if((dataD.className === `rotate-data`& dataD.style.color === `blue`)){
-            dataD.style.color = `black`;
+        }
+        if((dataD.className === `rotate-data`)){
             dataD.className = `reverse-rotate`;
         }
     });
 
     dataD.addEventListener(`click`, () => {
-        dataD.style.color = `blue`;
         dataD.className = `rotate-data`;
-        if((dataA.className === `rotate-data` & dataA.style.color === `blue`)){
-            dataA.style.color = `black`;
+        if((dataA.className === `rotate-data` )){
             dataA.className = `reverse-rotate`;
-        }else if((dataB.className === `rotate-data` & dataB.style.color === `blue`)){
-            dataB.style.color = `black`;
+        }
+        if((dataB.className === `rotate-data`)){
             dataB.className = `reverse-rotate`;
-        }else if((dataC.className === `rotate-data` & dataC.style.color === `blue`)){
-            dataC.style.color = `black`;
+        }if((dataC.className === `rotate-data`)){
             dataC.className = `reverse-rotate`;
         }
     });
